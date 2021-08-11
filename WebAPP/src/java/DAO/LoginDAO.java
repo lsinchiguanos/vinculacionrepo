28b8d5d43adea50ec5c41858f438702a691f1d11
jalmeidam2 <74794035+jalmeidam2@users.noreply.github.com>
2021-08-11 17:56:45 -0500

    Login
    Login validado enco y desco

--- WebAPP/src/java/DAO/LoginDAO.java
+++ WebAPP/src/java/DAO/LoginDAO.java
@@ -7,6 +7,9 @@ package DAO;
 
 import BD.conexion;
 import Model.Constructor;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.ResultSet;
 
 /**
@@ -14,17 +17,36 @@ import java.sql.ResultSet;
  * @author Manue
  */
 public class LoginDAO {
-    int rspta=0;
-    String sql="";
-    ResultSet rs=null;
-    conexion cn=new conexion();
-    public int validarLogin(Constructor tm) throws Exception{
-        sql="SELECT COUNT(galeno_id) AS cantidad FROM public.galeno WHERE galeno_user='"+tm.getUsuario()+"' AND galeno_pass='"+tm.getClave()+"'";
-        rs=cn.ejecutarConsulta(sql);
-        while(rs.next()){
-            rspta=rs.getInt("cantidad");
+
+    int rspta = 0;
+    String sql = "";
+    ResultSet rs = null;
+    conexion cn = new conexion();
+
+    public int validarLogin(Constructor tm) throws Exception {
+        String user = tm.getUsuario();
+        String clave1 = tm.getClave();
+        sql = "SELECT COUNT(galeno_id) AS cantidad FROM public.galeno WHERE galeno_user='" + user + "' AND galeno_pass='" + getMD5(clave1) + "'";
+        rs = cn.ejecutarConsulta(sql);
+        while (rs.next()) {
+            rspta = rs.getInt("cantidad");
         }
-    return  rspta;
+        return rspta;
     }
-    
+
+    public static String getMD5(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] messageDigest = md.digest(input.getBytes());
+            BigInteger number = new BigInteger(1, messageDigest);
+            String hashtext = number.toString(16);
+
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+            return hashtext;
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
