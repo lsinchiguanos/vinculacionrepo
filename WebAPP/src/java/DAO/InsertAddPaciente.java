20eb8919d907c6c651164ab1b7d451711ef96ab4
Leslie Alexander Sinchiguano Saltos <69779736+lsinchiguanos@users.noreply.github.com>
2021-09-08 12:07:41 -0500

    va

--- WebAPP/src/java/DAO/InsertAddPaciente.java
+++ WebAPP/src/java/DAO/InsertAddPaciente.java
@@ -22,14 +22,14 @@ public class InsertAddPaciente {
                 st.executeUpdate("INSERT INTO paciente(paciente_tipo_dni, paciente_dni, paciente_apellido_paterno, \n"
                         + "            paciente_primer_nombre, paciente_fnacimiento, paciente_direccion, \n"
                         + "            paciente_telefono, paciente_genero, paciente_grupo, paciente_ayudasocial, \n"
-                        + "            created_at, paciente_apellido_materno, paciente_segundo_nombre, \n"
-                        + "            paciente_estado, paciente_provincia, paciente_canton, paciente_parroquia, \n"
+                        + "            paciente_apellido_materno, paciente_segundo_nombre, \n"
+                        + "            paciente_provincia, paciente_canton, paciente_parroquia, \n"
                         + "            paciente_nacionalidad, paciente_estado_civil, paciente_convencional, \n"
                         + "            paciente_tipo_sangre,paciente_correoelectronico)VALUES ('" + usuario.getPaciente_tipo_dni() + "','" + usuario.getPaciente_dni() + "','" + usuario.getPaciente_apellido_paterno()
                         + "','" + usuario.getPaciente_primer_nombre() + "','" + usuario.getPaciente_fnacimiento() + "','" + usuario.getPaciente_direccion()
                         + "','" + usuario.getPaciente_telefono() + "','" + usuario.getPaciente_genero() + "','" + usuario.getPaciente_grupo()
-                        + "','" + usuario.getPaciente_ayudasocial() + "','" + usuario.getCreated_at() + "','" + usuario.getPaciente_apellido_materno()
-                        + "','" + usuario.getPaciente_segundo_nombre() + "','" + usuario.getPaciente_estado() + "','" + usuario.getPaciente_provincia()
+                        + "','" + usuario.getPaciente_ayudasocial() + "','" + usuario.getPaciente_apellido_materno()
+                        + "','" + usuario.getPaciente_segundo_nombre() + "','" + usuario.getPaciente_provincia()
                         + "','" + usuario.getPaciente_canton() + "','" + usuario.getPaciente_parroquia() + "','" + usuario.getPaciente_nacionalidad()
                         + "','" + usuario.getPaciente_estado_civil() + "','" + usuario.getPaciente_convencional() + "','" + usuario.getPaciente_tipo_sangre()
                         + "','" + usuario.getPaciente_email() + "')");
